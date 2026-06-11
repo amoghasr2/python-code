cf58230070d65c4e4e3e8fc6abd3ce9b2880998d
amogha <sraoamogh08@gmail.com>
2026-06-11 07:48:42 +0530

    version-2

--- p1.py
+++ p1.py
@@ -3,3 +3,15 @@ if str(a)==str(a)[::-1]:
     print(a,"is a palindrome")
 else:
     print(a,"is not a palindrome")
+
+
+n = list(range(-25,26))
+print(n)
+negative = [num for num in n if num<0]
+print(negative)
+positive = [num for num in n if num>0]
+print(positive)
+zero = [num for num in n if num==0]
+print(zero)
+
+
